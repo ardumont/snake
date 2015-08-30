6e1a24869af45fa7268642e14981c1ed8036d556
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-08-30 21:46:38 +0200

    Refactor - Clarify code

--- app/Main.hs
+++ app/Main.hs
@@ -29,11 +29,9 @@ circularSnakeWorld _ (World (Snake dir coord) apple limit) =
   World updatedSnake apple limit
   where updatedSnake = Snake dir (circularSnake coord limit)
 
--- data example
--- apple_1 :: Apple
--- apple_1 = Apple { _coorda = Coord 10 10 }
--- snake_1 = Snake { _direction = DirUp,
---                   _coords = Coord 10 10 }
+-- | Snake eats apple
+snakeEatsApple :: t -> World -> World
+snakeEatsApple _ world = undefined
 
 -- This starts our game in a window with a give size, running at 30 frames per second.
 -- The argument 'World (0, 0)' is the initial state of our game world, where our character is at the centre of the
@@ -50,9 +48,18 @@ circularSnakeWorld _ (World (Snake dir coord) apple limit) =
 --      -> IO ()
 
 main :: IO ()
-main = play (InWindow "Snake" (w, h) (50, 50)) white 30 world drawWorld handle [ nextMoveWorld, circularSnakeWorld ]
-       where snake = Snake DirUp (Coord 0 0)
-             apple = Apple (Coord 10 10)
+main = play (InWindow "Snake" (w, h) (0, 0)) -- Window display
+         white                               -- Color
+         100                                 -- frame per second
+         world                               --
+         drawWorld                           -- draw the world function
+         handleUserInput                     -- Handle user input
+         [ nextMoveWorld,                    -- snake moves
+           circularSnakeWorld,               -- can move out of bounds
+           snakeEatsApple                    -- snake eats apple
+         ]
+       where snake = Snake DirRight (Coord 0 0)
+             apple = Apple (Coord 100 100)
              world = World snake apple (Limit width height)
              w = round width
              h = round height
@@ -60,31 +67,19 @@ main = play (InWindow "Snake" (w, h) (50, 50)) white 30 world drawWorld handle [
 -- To draw a frame, we position the character sprite at the location as determined by the current state of the world.
 -- We shrink the sprite by 50%.
 
+-- | Draw the pix at coordinates
 drawCoordinates :: Coord -> Picture -> Picture
-drawCoordinates (Coord x y) pix = translate x y (scale 0.5 0.5 pix)
+drawCoordinates (Coord x y) pix = translate x y (scale 1 1 pix)
 
+-- | Draw the world
 drawWorld :: World -> Picture
 drawWorld (World (Snake _ cx) (Apple ca) _) =
   pictures [ drawCoordinates cx snakePix, drawCoordinates ca applePix ]
 
--- Char :: Char -> Key
--- SpecialKey :: SpecialKey -> Key
--- EventKey
---   :: Key
---      -> KeyState
---      -> gloss-1.9.4.1:Graphics.Gloss.Internals.Interface.Backend.Types.Modifiers
---      -> (Float, Float)
---      -> Event
-
--- Whenever any of the keys 'a', 'd', 'w', or 's' have been pushed down, move our character in the corresponding
--- direction.
-handle :: Event -> World -> World
-handle (EventKey (Char 'a') Down _ _) (World (Snake _ c) a l) = World (Snake DirLeft c) a l
-handle (EventKey (Char 'd') Down _ _) (World (Snake _ c) a l) = World (Snake DirRight c) a l
-handle (EventKey (Char 'w') Down _ _) (World (Snake _ c) a l) = World (Snake DirUp c) a l
-handle (EventKey (Char 's') Down _ _) (World (Snake _ c) a l) = World (Snake DirDown c) a l
-handle (EventKey (SpecialKey KeyLeft) Down _ _)  (World (Snake _ c) a l) = World (Snake DirLeft c) a l
-handle (EventKey (SpecialKey KeyRight) Down _ _) (World (Snake _ c) a l) = World (Snake DirRight c) a l
-handle (EventKey (SpecialKey KeyUp) Down _ _)    (World (Snake _ c) a l) = World (Snake DirUp c) a l
-handle (EventKey (SpecialKey KeyDown) Down _ _)  (World (Snake _ c) a l) = World (Snake DirDown c) a l
-handle _ world = world        -- don't change the world in case of any other events
+-- | Handle keybindings to change the snake's direction according to keys
+handleUserInput :: Event -> World -> World
+handleUserInput (EventKey (SpecialKey KeyLeft) Down _ _)  (World (Snake _ c) a l) = World (Snake DirLeft c) a l
+handleUserInput (EventKey (SpecialKey KeyRight) Down _ _) (World (Snake _ c) a l) = World (Snake DirRight c) a l
+handleUserInput (EventKey (SpecialKey KeyUp) Down _ _)    (World (Snake _ c) a l) = World (Snake DirUp c) a l
+handleUserInput (EventKey (SpecialKey KeyDown) Down _ _)  (World (Snake _ c) a l) = World (Snake DirDown c) a l
+handleUserInput _ world = world        -- don't change the world in other events
