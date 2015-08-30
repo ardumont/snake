795eafc48fc98ea3d4b22303997b90336464194f
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-08-30 11:00:14 +0200

    Revert "Use cabal2nix to compile"
    This reverts commit dbb6cafde03c52ca8b65f3c7cf0f1bfbac08d145.

--- default.nix
+++ default.nix
@@ -1,2 +1,22 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7101" }:
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./snake.nix { }
+let pkgs = import <nixpkgs> {};
+in with pkgs; rec {
+  snakeEnv = stdenv.mkDerivation {
+    name = "env-snake";
+    buildInputs = with haskellngPackages; [
+      # c deps
+      xorg_sys_opengl mesa_glu freeglut
+      # haskell's bare deps
+      cabal-install
+      # not really needed
+    #  cabal2nix lens hlint hdevtools zlib mtl HUnit QuickCheck hoogle
+      ];
+    # CABAL_INSTALL_EXTRA_FLAGS = ''
+    #     --extra-lib-dirs=${xorg_sys_opengl}/lib \
+    #     --extra-lib-dirs=${mesa_glu}/lib \
+    #     --extra-lib-dirs=${freeglut}/lib \
+    #     --extra-include-dirs=${xorg_sys_opengl}/include \
+    #     --extra-include-dirs=${mesa_glu}/include \
+    #     --extra-include-dirs=${freeglut}/include\
+    # '';
+  };
+}
