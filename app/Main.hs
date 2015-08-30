b353a6b73b87817f8c742ff904f51cfefae7a389
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-08-30 16:15:35 +0200

    Refactor - Simplify code

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,6 @@
-{-# LANGUAGE NoMonomorphismRestriction #-}
-{-# OPTIONS_GHC -fno-full-laziness -fno-cse #-}
 module Main where
 
 import           Lib
-
-import           Snake.Move
-import           Snake.Type
 
 width :: Int
 width = 1000
@@ -13,16 +8,7 @@ width = 1000
 height :: Int
 height = 1000
 
-Data World = World Snake Apple (Limit width height)
-
--- | From random int to direction
-toDirection :: (Eq a, Num a) => a -> Direction
-toDirection 1 = DirUp
-toDirection 2 = DirRight
-toDirection 3 = DirDown
-toDirection 4 = DirLeft
-toDirection _ = error "Only 4 possible values are 1, 2, 3, 4."
-
+data World = World Snake Apple Limit
 
 main :: IO ()
 main = undefined
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,65 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Lib
-    ( someFunc
+    ( nextMove
+    , circularSnake
+    , collision
+    , Direction
+    , Coord
+    , Apple
+    , Snake
+    , Limit
     ) where
 
-import           Snake.Move
-import           Snake.Type
+import           Control.Lens
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+-- | Snake's possible direction
+data Direction = DirUp | DirRight | DirDown | DirLeft
+                 deriving (Eq, Show)
+
+-- | Entities have coordinates
+data Coord = Coord Int Int deriving (Eq, Show)
+
+-- | Apple
+data Apple = Apple { _coorda :: Coord }
+             deriving (Eq, Show)
+
+-- | Snake eats apple
+data Snake = Snake { _direction :: Direction, _coords :: Coord }
+             deriving (Eq, Show)
+
+makeLenses ''Apple
+makeLenses ''Snake
+
+-- | World has limits
+data Limit = Limit Int Int
+
+-- | From a direction and a set of coordinates, compute the next possible
+-- coordinates
+nextMove :: Direction -> Coord -> Coord
+nextMove DirUp (Coord x y) = Coord x (1 + y)
+nextMove DirRight (Coord x y) = Coord (1 + x) y
+nextMove DirDown (Coord x y) = Coord x (y - 1)
+nextMove DirLeft (Coord x y) = Coord (x - 1) y
+
+-- | Given a set of coordinates and a limit, determine the next possible
+-- coordinates.
+circularSnake :: Coord -> Limit -> Coord
+circularSnake (Coord x y) (Limit w h) =
+  case (x < 0, x > w, y < 0, y > h) of
+     (True, _, _, _ ) -> Coord w y
+     (_, True, _, _)  -> Coord 0 y
+     (_, _, True, _)  -> Coord x h
+     (_, _, _, True)  -> Coord x 0
+     _                -> Coord x y
+
+-- | Given two sets of coordinates, determine if there is a collision or not
+collision :: Coord -> Coord -> Bool
+collision = (==)
+
+-- | From random int to direction
+toDirection :: (Eq a, Num a) => a -> Direction
+toDirection 1 = DirUp
+toDirection 2 = DirRight
+toDirection 3 = DirDown
+toDirection 4 = DirLeft
+toDirection _ = error "Only 4 possible values are 1, 2, 3, 4."
