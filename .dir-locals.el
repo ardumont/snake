4eb0e03993cdc3c09a7d6040550b719ee90b5007
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-09-04 17:51:41 +0200

    Update on .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,3 @@
-((haskell-mode . ((haskell-process-wrapper-function . (lambda (argv) (append (list "nix-shell" "shell.nix" "--command")
+((haskell-mode . ((haskell-process-wrapper-function . (lambda (argv) (append (list "nix-shell" "-I" "." "--command")
                                                                         (list (mapconcat 'identity argv " ")))))
                   (haskell-process-type . cabal-repl))))
